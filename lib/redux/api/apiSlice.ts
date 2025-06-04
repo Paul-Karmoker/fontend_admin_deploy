60d46843c667d50d486ce7562445ecf8c33b7f13
ibrahim23dev <ibrahim.soft.dev23@gmail.com>
2025-06-05 02:04:00 +0600

    update base URL in apiSlice to production server

--- lib/redux/api/apiSlice.ts
+++ lib/redux/api/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import type { RootState } from "../store"
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:4001",
+  baseUrl: "https://backend-server-deploy.onrender.com",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token
     if (token) {
