63ac831fcf8badf378e58f3a74629b2485635568
ibrahim23dev <ibrahim.soft.dev23@gmail.com>
2025-08-26 18:05:08 +0600

    upload

--- lib/redux/api/apiSlice.ts
+++ lib/redux/api/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import type { RootState } from "../store"
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://backend-server-deploy.onrender.com",
+  baseUrl: "https://api.crosscareers.com",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token
     if (token) {
